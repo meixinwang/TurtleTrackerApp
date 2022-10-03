fb459daa4e5ccd3053ea3c4aa250fd8269591873
Meixin Wang <mw510@duke.edu>
2022-10-02 22:59:50 -0400

    Initial commit of ARGOSTrackingTool script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+# 
+# Description: Reads in an ARGOS tracking data file and 
+# allows the user to view the location of the turtle
+# for a specified date entered via user input.
+
+# Author: Meixin Wang (meixin.wang@duke.edu)
+# Date: Fall 2022
+#--------------------------------------------------------------
+
+
