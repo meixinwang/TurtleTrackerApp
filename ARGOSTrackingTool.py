fa1bca9f368af0535e67810acd7b14c83a59859c
Meixin Wang <mw510@duke.edu>
2022-10-12 10:46:45 -0400

    get locations from user input date

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -10,31 +10,34 @@
 # Date: Fall 2022
 #--------------------------------------------------------------
 
-#Create a variable pointing to the data file
+# Ask the user for a date, specifying the format
+user_date = input("Enter a date (M/D/YYYY):")
+
+# Create a variable pointing to the data file
 file_name = 'data/raw/Sara.txt'
 
-#Create a file object from the file
+# Create a file object from the file
 file_object = open(file_name,'r')
 
-#Read contents of file into a list
+# Read contents of file into a list
 lineList = file_object.readlines()
 file_object.close()
 
-# create empty dictionaries for date and location data, key recordID
+# Create empty dictionaries for date and location data, key recordID
 date_dict = {}
 location_dict = {}
 
-#Extract one data line into a variable
+# Extract one data line into a variable
 for lineString in lineList:
     
-    #Check to see if the line is a data line
+    # Check to see if the line is a data line
     if lineString[0] in ("#", "u"):
         continue
     
-    #Split the string into a list of data items
+    # Split the string into a list of data items
     lineData = lineString.split()
     
-    #Extract items in list into variables
+    # Extract items in list into variables
     record_id = lineData[0]
     obs_date = lineData[2]
     obs_lc = lineData[4]
@@ -48,3 +51,17 @@ for lineString in lineList:
         
         date_dict[record_id] = obs_date
         location_dict[record_id] = (obs_lat, obs_lon)
+        
+# Create an empty key list
+matching_keys = []
+
+# Loop through all key-value pairs in the date dictionary
+for the_key, the_value in date_dict.items():
+    # Check if the date matches the user date
+    if the_value == user_date:
+        matching_keys.append(the_key)
+        
+# Reveal locations for each key in matching_keys
+for matching_key in matching_keys:
+    obs_lat, obs_lon = location_dict[matching_key]
+    print(f"Record {matching_key} indicates Sara was seen at lat:{obs_lat}, lon:{obs_lon} on {user_date}")
